415daa0bd782f03081ae02df4be5fbb0beb895ad
codingpalette <codingpalette@gmail.com>
2022-08-31 22:02:27 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e42dc7608ef4e4c2d482.js"
+    "url": "webpack-runtime-5b089e0b95575018bbdd.js"
   },
   {
     "url": "styles.c75940ba2e9d08c2bb15.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cb1c986a367b599c8c7108b842f73986"
+    "revision": "d1ae9c6bf6152d9540ec0a1cd98540a5"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
